a69f6df78f4ff941432d25787a12d8baf4093e29
Rewer172 <privetdiablo3@mail.ru>
2018-11-15 15:43:01 +0300

    WebApp

---
+++ RazorWebApp/Controllers/NotesController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorWebApp.Models;
+using RazorWebApp.Data;
+using Microsoft.AspNetCore.Authorization; //для функций авторизации
+
+
+namespace RazorWebApp.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notes
+        [Authorize]
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var notes = from m in _context.Note
+                        select m;
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                notes = notes.Where(s => s.HeadLine.Contains(searchString));
+            }
+            return View(await notes.ToListAsync());
+        }
+
+        // GET: Notes/Details/5
+        [Authorize]
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var note = await _context.Note
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return View(note);
+        }
+
+        // GET: Notes/Create
+        [Authorize]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Notes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,HeadLine,Tag,Data,Number")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(note);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(note);
+        }
+
+        // GET: Notes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var note = await _context.Note.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            return View(note);
+        }
+
+        // POST: Notes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,HeadLine,Tag,Data,Number")] Note note)
+        {
+            if (id != note.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(note);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NoteExists(note.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(note);
+        }
+
+        // GET: Notes/Delete/5
+        [Authorize]
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var note = await _context.Note
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return View(note);
+        }
+
+        // POST: Notes/Delete/5
+        [Authorize]
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var note = await _context.Note.FindAsync(id);
+            _context.Note.Remove(note);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NoteExists(int id)
+        {
+            return _context.Note.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ RazorWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RazorWebApp.Models;
+
+namespace RazorWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<RazorWebApp.Models.Note> Note { get; set; }
+    }
+}
